cabf715190d4ff2cf92efaa7c186163e3c803149
adamtlee <adam.lee346@gmail.com>
2021-01-19 22:13:55 -0700

    added entity models

--- CoffeeBox.Data/Migrations/20210120050543_ProductInventorySales.Designer.cs
+++ CoffeeBox.Data/Migrations/20210120050543_ProductInventorySales.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using CoffeeBox.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace CoffeeBox.Data.Migrations
 {
     [DbContext(typeof(CoffeeBoxDbContext))]
-    partial class CoffeeBoxDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210120050543_ProductInventorySales")]
+    partial class ProductInventorySales
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
