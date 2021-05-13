0923551afc4d42559959a8da11b30397a9f8523e
adamtlee <adam.lee346@gmail.com>
2021-05-12 22:14:53 -0600

    Added more test cases

---
+++ CoffeeBox.Test/TestInventoryService.cs
@@ -0,0 +1,27 @@
+﻿using CoffeeBox.Data;
+using CoffeeBox.Services.Inventory;
+using CoffeeBox.Services.Product;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CoffeeBox.Test
+{
+    public class TestInventoryService
+    {
+        [Fact]
+        public void InventoryService_GetsAllInventory_GivenTheyExist()
+        {
+            var options = new DbContextOptionsBuilder<CoffeeBoxDbContext>()
+                .UseInMemoryDatabase("gets_all").Options;
+
+            using var context = new CoffeeBoxDbContext(options);
+            var sut = new ProductService(context);
+        }
+    }
+}
---
+++ CoffeeBox.Test/TestProductService.cs
@@ -0,0 +1,36 @@
+﻿using CoffeeBox.Data;
+using CoffeeBox.Services.Product;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CoffeeBox.Test
+{
+    public class TestProductService
+    {
+        [Fact]
+        public void ProductService_GetsAllProducts_GivenTheyExist()
+        {
+            var options = new DbContextOptionsBuilder<CoffeeBoxDbContext>()
+                .UseInMemoryDatabase("gets_all").Options;
+
+            using var context = new CoffeeBoxDbContext(options);
+
+            var sut = new ProductService(context);
+
+            sut.CreateProduct(new Data.Models.Product { Id = 1 });
+            sut.CreateProduct(new Data.Models.Product { Id = 2 });
+
+            var allProducts = sut.GetAllProducts();
+
+            allProducts.Count.Should().Be(2); 
+
+
+        }
+    }
+}
