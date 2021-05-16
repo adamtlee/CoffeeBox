78378a8ccaa652c5b9198efc3199b6acda42f61b
adamtlee <adam.lee346@gmail.com>
2021-05-15 22:20:37 -0600

    Product Service Test cases

--- CoffeeBox.Test/TestProductService.cs
+++ CoffeeBox.Test/TestProductService.cs
@@ -1,4 +1,5 @@
 ﻿using CoffeeBox.Data;
+using CoffeeBox.Data.Models;
 using CoffeeBox.Services.Product;
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
@@ -28,9 +29,49 @@ namespace CoffeeBox.Test
 
             var allProducts = sut.GetAllProducts();
 
-            allProducts.Count.Should().Be(2); 
+            allProducts.Count.Should().Be(2);
+        }
+        [Fact]
+        public void ProductService_CreatesProduct_GivenNewProductObject()
+        {
+            var options = new DbContextOptionsBuilder<CoffeeBoxDbContext>()
+                .UseInMemoryDatabase("Archives_product").Options;
 
+            using var context = new CoffeeBoxDbContext(options);
+            var sut = new ProductService(context);
 
+            sut.CreateProduct(new Product { Id = 7689 });
+            context.Products.Single().Id.Should().Be(7689);
+        }
+        [Fact]
+        public void ProductService_ArchivesProduct_GivenNewProductObject()
+        {
+            var options = new DbContextOptionsBuilder<CoffeeBoxDbContext>()
+                .UseInMemoryDatabase("Add_writes_to_database").Options;
+
+            using var context = new CoffeeBoxDbContext(options);
+            var sut = new ProductService(context);
+
+            sut.CreateProduct(new Product { Id = 7 });
+            sut.ArchiveProduct(7);
+            var archivedProduct = sut.GetProductById(7);
+            archivedProduct.IsArchived.Should().Be(true);
+        }
+
+        [Fact]
+        public void ProductService_GetProductById_GivenTheyExist()
+        {
+            var options = new DbContextOptionsBuilder<CoffeeBoxDbContext>()
+                .UseInMemoryDatabase("gets_one").Options;
+
+            using var context = new CoffeeBoxDbContext(options);
+            var sut = new ProductService(context);
+
+            sut.CreateProduct(new Product { Id = 7 });
+            sut.CreateProduct(new Product { Id = 24 });
+
+            var getProductWithId = sut.GetProductById(24);
+            getProductWithId.Id.Should().Be(24);
         }
     }
 }
